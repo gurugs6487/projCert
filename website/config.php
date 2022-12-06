eec6431ba139511becc18f0a1899036c6de5a047
Guruprasath Gajavalli Sarvothaman <guruprasath.gs@tcs.com>
2022-12-06 00:44:45 +0000

    config.php changed to have my name

--- website/config.php
+++ website/config.php
@@ -8,7 +8,7 @@
 function config($key = '')
 {
     $config = [
-        'name' => 'Simple PHP Website',
+        'name' => 'Simple PHP Website By Guruprasath Sarvothaman',
         'nav_menu' => [
             '' => 'Home',
             'content/about-us.php' => 'About Us',
